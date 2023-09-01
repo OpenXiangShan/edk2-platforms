3ea379568a2eb16c7580491a4aac65e240ee4929
Guo Yaxing <guoyaxing@bosc.ac.cn>
2023-09-01 13:57:39 +0800

    Divide NanhuFPGABoard into NanhuFPGABoard and NanhuFPGABoardDualPcie in Platform.
    We have two commonly used bit stream -- regression and dual-xdma.
    They have different base adress in xdma. So we separate them into two Platform.
    
    Signed-off-by: Guo Yaxing <guoyaxing@bosc.ac.cn>

--- Platform/Bosc/XiangshanSeriesPkg/NanhuFPGABoard/AcpiTables/Dsdt/Pci.asl
+++ Platform/Bosc/XiangshanSeriesPkg/NanhuFPGABoard/AcpiTables/Dsdt/Pci.asl
@@ -18,39 +18,9 @@ Scope(_SB)
       QWordMemory (  // 64-bit BAR Windows
         ResourceProducer, PosDecode, MinFixed, MaxFixed, NonCacheable, ReadWrite,
         0,                             // Granularity
-        0x0000000020000000,            // Min Base Address
-        0x0000000027FFFFFF,            // Max Base Address
-        0x0000000020000000,            // Translate
-        0x0000000008000000,            // Length
-      )
-    })
-
-    // Root complex resources
-    Method (_CRS, 0, Serialized) {
-      Return (RBUF)
-    }
-  }
-  Device (PCI1) {
-    Name (_HID, "PNP0A08") // PCI Express Root Bridge
-    Name (_CID, "PNP0A03") // Compatible PCI Root Bridge
-    Name(_SEG, 1) // Segment of this Root complex
-    Name(_BBN, 0) // Base Bus Number
-    Name(_CCA, 1) // Cache Coherency Attribute
-    Name(RBUF, ResourceTemplate () {
-     WordBusNumber ( // Bus numbers assigned to this root
-        ResourceProducer, MinFixed, MaxFixed, PosDecode,
-        0,        // AddressGranularity
-        0,        // AddressMinimum - Minimum Bus Number
-        2,     // AddressMaximum - Maximum Bus Number
-        0,        // AddressTranslation
-        3,    // RangeLength - # of Busses
-      )
-      QWordMemory (  // 64-bit BAR Windows
-        ResourceProducer, PosDecode, MinFixed, MaxFixed, NonCacheable, ReadWrite,
-        0,                             // Granularity
-        0x0000000040000000,            // Min Base Address
-        0x000000005FFFFFFF,            // Max Base Address
-        0x0000000020000000,            // Translate
+        0x0000000060000000,            // Min Base Address
+        0x000000007FFFFFFF,            // Max Base Address
+        0x0000000000000000,            // Translate
         0x0000000020000000,            // Length
       )
     })
---
+++ Platform/Bosc/XiangshanSeriesPkg/NanhuFPGABoardDualPcie/AcpiTables/AcpiHeader.h
@@ -0,0 +1,62 @@
+/** @file
+ *
+ *  Defines for constructing ACPI tables
+ *
+ *  Copyright (c) 2020, Pete Batard <pete@akeo.ie>
+ *  Copyright (c) 2019, ARM Ltd. All rights reserved.
+ *  Copyright (c) 2018, Andrei Warkentin <andrey.warkentin@gmail.com>
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *
+ *  SPDX-License-Identifier: BSD-2-Clause-Patent
+ *
+ **/
+
+#ifndef __ACPIHEADER_H__
+#define __ACPIHEADER_H__
+
+#include <IndustryStandard/Acpi.h>
+
+#define EFI_ACPI_OEM_ID                       {'B','O','S','C',' ',' '}
+#define EFI_ACPI_OEM_TABLE_ID                 SIGNATURE_64 ('N','A','N','H','U',' ',' ',' ')
+#define EFI_ACPI_OEM_REVISION                 0x00000001
+#define EFI_ACPI_CREATOR_ID                   SIGNATURE_32 ('E','D','K','2')
+#define EFI_ACPI_CREATOR_REVISION             0x00000001
+
+///
+/// RHCT Revision
+///
+#define EFI_ACPI_6_0_MULTIPLE_RHCT_DESCRIPTION_TABLE_REVISION  0x01
+
+///
+/// "RHCT" Rhct Description Table
+///
+#define EFI_ACPI_6_0_MULTIPLE_RHCT_DESCRIPTION_TABLE_SIGNATURE  SIGNATURE_32('R', 'H', 'C', 'T')
+
+#define ACPI_HEADER(Signature, Type, Revision) {                  \
+    Signature,                      /* UINT32  Signature */       \
+    sizeof (Type),                  /* UINT32  Length */          \
+    Revision,                       /* UINT8   Revision */        \
+    0,                              /* UINT8   Checksum */        \
+    EFI_ACPI_OEM_ID,                /* UINT8   OemId[6] */        \
+    EFI_ACPI_OEM_TABLE_ID,          /* UINT64  OemTableId */      \
+    EFI_ACPI_OEM_REVISION,          /* UINT32  OemRevision */     \
+    EFI_ACPI_CREATOR_ID,            /* UINT32  CreatorId */       \
+    EFI_ACPI_CREATOR_REVISION       /* UINT32  CreatorRevision */ \
+  }
+
+#define EFI_ACPI_INTC_INIT(HartId, Uid) {                      \
+    0x18,                   /* UINT8   Type */                 \
+    20,                     /* UINT8   Length */               \
+    1,                      /* UINT8   Version */              \
+    0,                      /* UINT8   Reserved*/              \
+    1,                      /* UINT32  Flags */                \
+    HartId,                 /* UINT64  Hart ID */              \
+    Uid                     /* UINT32  ACPI Processor UID*/    \
+  }
+
+///
+/// MSI
+///
+#define XDMA_MSI_IRQ_CONTROLLER 0x0
+
+#endif
